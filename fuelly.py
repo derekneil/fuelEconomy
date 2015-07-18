2a91da57c97b1a86cc68f01a936b059b0abfd257
Derek Neil <derekrneil@gmail.com>
2015-07-18 19:35:35 -0300

    update main script to only do deep scraping, added all nationalities, save each model in new file for online analysis

--- fuelly.py
+++ fuelly.py
@@ -9,33 +9,6 @@ DEBUG = 1
 url = 'http://www.fuelly.com/car'
 destination = 'fuelly-car-data'
 
-#Sample model listing
-'''
-<div class="row">
-	<div class="col-sm-12 col-md-12">			
-						<h4 class="make-header"><a class="make-header-link" href="http://www.fuelly.com/car/abarth">Abarth</a></h4>
-			<ul class="models-list">
-								<li><a href="http://www.fuelly.com/car/abarth/1000">1000</a> (1)</li>
-								<li><a href="http://www.fuelly.com/car/abarth/500">500</a> (69)</li>
-								<li><a href="http://www.fuelly.com/car/abarth/750">750</a> (1)</li>
-								<li><a href="http://www.fuelly.com/car/abarth/assetto">Assetto</a> (1)</li>
-								<li><a href="http://www.fuelly.com/car/abarth/gnasher">Gnasher</a> (1)</li>
-								<li><a href="http://www.fuelly.com/car/abarth/grande_punto">Grande Punto</a> (3)</li>
-								<li><a href="http://www.fuelly.com/car/abarth/punto_evo">Punto Evo</a> (5)</li>
-								<li><a href="http://www.fuelly.com/car/abarth/punto_supersport">Punto Supersport</a> (1)</li>
-							</ul>
-						<h4 class="make-header"><a class="make-header-link" href="http://www.fuelly.com/car/ac">AC</a></h4>
-			<ul class="models-list">
-								<li><a href="http://www.fuelly.com/car/ac/427">427</a> (3)</li>
-							</ul>
-						<h4 class="make-header"><a class="make-header-link" href="http://www.fuelly.com/car/acura">Acura</a></h4>
-			<ul class="models-list">
-								<li><a href="http://www.fuelly.com/car/acura/cl">CL</a> (56)</li>
-								<li><a href="http://www.fuelly.com/car/acura/csx">CSX</a> (40)</li>
-								<li><a href="http://www.fuelly.com/car/acura/el">EL</a> (81)</li>
-
-'''
-
 def safeInt(s):
 	try:
 		s = s.replace(',','')
@@ -46,26 +19,237 @@ def safeInt(s):
 		return s
 
 nationalities = {
+	1:'Andorra',
+	2:'United Arab Emirates',
+	3:'Afghanistan',
+	4:'Antigua and Barbuda',
+	5:'Anguilla',
+	6:'Albania',
+	7:'Armenia',
+	8:'Netherlands Antilles',
+	9:'Angola',
+	10:'Antarctica',
+	11:'Argentina',
+	12:'American Samoa',
+	13:'Austria',
 	14:'Australia',
+	15:'Aruba',
+	16:'Azerbaijan',
+	17:'Bosnia and Herzegovina',
+	18:'Barbados',
+	19:'Bangladesh',
+	20:'Belgium',
+	21:'Burkina Faso',
+	22:'Bulgaria',
+	23:'Bahrain',
+	24:'Burundi',
+	25:'Benin',
+	26:'Bermuda',
+	27:'Brunei',
+	28:'Bolivia',
 	29:'Brazil',
+	30:'Bahamas',
+	31:'Bhutan',
+	33:'Botswana',
+	34:'Belarus',
+	35:'Belize',
 	36:'Canada',
+	37:'Cocos Islands',
+	38:'Congo',
+	39:'Central African Republic',
+	40:'Brazzaville',
+	41:'Switzerland',
+	42:'Ivory Coast',
+	43:'Cook Islands',
+	44:'Chile',
+	45:'Cameroon',
 	46:'China',
+	47:'Colombia',
+	48:'Costa Rica',
+	49:'Cuba',
+	50:'Cape Verde',
+	51:'Christmas Island',
+	52:'Cyprus',
+	53:'Czech Republic',
+	54:'Germany',
+	55:'Djibouti',
+	56:'Denmark',
+	57:'Dominica',
+	58:'Dominican Republic',
+	59:'Algeria',
+	60:'Ecuador',
+	61:'Estonia',
+	62:'Egypt',
+	63:'Western Sahara',
+	64:'Eritrea',
+	65:'Spain',
+	66:'Ethiopia',
 	67:'Finland',
+	68:'Fiji',
+	69:'Falkland Islands',
+	71:'Faroe Islands',
+	72:'France',
+	74:'Gabon',
+	75:'Grenada',
+	76:'Georgia',
+	77:'Gviana',
+	78:'Guernsey',
+	79:'Ghana',
+	80:'Gibraltar',
+	81:'Greenland',
+	82:'Gambia',
+	83:'Guinea',
+	84:'Equatorial Guinea',
 	86:'Greece',
+	88:'Guatemala',
+	89:'Guam',
+	90:'Guinea Bissau',
+	91:'Guyana',
+	92:'Hong Kong',
+	94:'Honduras',
+	95:'Croatia',
+	96:'Haiti',
+	97:'Hungary',
+	98:'Indonesia',
 	99:'Ireland',
+	100:'Israel',
+	101:'Isle of Man',
+	102:'India',
+	104:'Iraq',
+	105:'Iran',
+	106:'Iceland',
+	107:'Italy',
+	108:'Jersey',
+	109:'Jamaica',
+	110:'Jordan',
+	111:'Japan',
+	112:'Kenya',
+	113:'Kyrgyzstan',
+	114:'Cambodia',
+	115:'Kiribati',
+	116:'Comoros',
+	117:'Saint Kitts and Nevis',
+	119:'South Korea',
+	120:'Kuwait',
+	121:'Cayman Islands',
+	122:'Kazakhstan',
+	123:'Laos',
+	124:'Lebanon',
+	125:'Saint Lucia',
+	126:'Liechtenstein',
+	127:'Sri Lanka',
+	128:'Liberia',
+	129:'Lesotho',
+	130:'Lithuania',
+	131:'Luxembourg',
+	132:'Latvia',
+	133:'Libya',
+	134:'Morocco',
+	135:'Monaco',
+	136:'Moldova',
+	137:'Madagascar',
+	138:'Marshall Islands',
+	139:'Macedonia',
+	140:'Mali',
+	141:'Myanmar',
+	142:'Mongolia',
+	143:'Macau',
+	144:'Northern Mariana Islands',
+	145:'Martinique',
+	146:'Mauritania',
+	147:'Montserrat',
+	148:'Malta',
+	149:'Mauritius',
+	150:'Maldives',
+	151:'Malawi',
 	152:'Mexico',
 	153:'Maylasia',
+	154:'Mozambique',
+	155:'Namibia',
+	156:'New Caledonia',
+	157:'Niger',
+	158:'Norfolk Island',
+	159:'Nigeria',
+	160:'Nicaragua',
+	161:'Netherlands',
 	162:'Norway',
+	163:'Nepal',
+	164:'Nauru',
+	165:'Niue',
 	166:'New Zealand',
+	167:'Oman',
+	168:'Panama',
+	169:'Peru',
+	170:'French Polynesia',
+	171:'Papua New Guinea',
+	172:'Philippines',
+	173:'Pakistan',
+	174:'Poland',
+	175:'Saint Pierre and Miquelon ',
+	176:'Pitcairn Islands',
+	177:'Puerto Rico',
+	178:'Palestine',
 	179:'Portugal',
+	180:'Palau',
+	181:'Paraguay',
+	182:'Qatar',
+	184:'Romania',
+	185:'Russia',
+	186:'Rwanda',
+	187:'Saudi Arabia',
+	188:'Solomon Islands',
+	189:'Seychelles',
+	190:'Sudan',
 	191:'Sweden',
+	192:'Singapore',
+	193:'Saint Helena',
+	194:'Slovenia',
+	195:'Norway',
+	196:'Slovakia',
+	197:'Sierra Leone',
+	198:'San Marino',
+	199:'Senegal',
+	200:'Somalia',
+	201:'Suriname',
+	202:'Sao Tome and Principe',
+	203:'El Salvador',
+	204:'Syria',
+	205:'Swaziland',
+	206:'Turks and Caicos Islands',
+	207:'Chad',
+	209:'Togo',
 	210:'Thailand',
+	211:'Tajikistan',
+	212:'Tokelau',
+	213:'Turkmenistan',
+	214:'Tunisia',
+	215:'Tonga',
+	216:'Timor Leste',
 	217:'Turkey',
+	218:'Trinidad and Tobago',
+	219:'Tuvalu',
 	220:'Taiwan',
+	221:'Tanzania',
 	222:'Ukraine',
+	223:'Uganda ',
 	224:'United Kingdom',
 	226:'United States',
-	241:'South Africa'
+	227:'Uruguay',
+	228:'Uzbekistan',
+	230:'Saint Vincent and The Grenadines ',
+	231:'Venezuela',
+	232:'British Virgin Islands',
+	233:'United States Virgin Islands',
+	234:'Vietnam',
+	235:'Vanuatu',
+	236:'Wallis and Futuna',
+	237:'Samoa',
+	238:'Yemen',
+	241:'South Africa',
+	242:'Zambia',
+	243:'Zimbabwe',
+	244:'Serbia',
+	245:'Montenegro'
 	}
 
 curbWeightRE = re.compile('(\d+) lbs',flags=re.IGNORECASE)
@@ -79,37 +263,28 @@ def api(make, model, year):
 	url="http://www.edmunds.com/"+make+"/"+model+"/"+year+"/features-specs/"
 	
 	try:
-		if DEBUG: print '\tedmunds api call to',url
+		if DEBUG: print '\t\tedmunds api call to',url
 		modelFeatures = pq(url=url)
 		
 		for highlight in modelFeatures("#highlights-pod .data-table li").items():
-# 			if DEBUG: print 
-# 	 		if DEBUG: print highlight.html()
 			span = highlight("span").text()
-# 			if DEBUG: print 'span:',span
 			
 			if "CAR TYPE" in span:
 				type = highlight("em").text().strip().upper()
-# 				if DEBUG: print 'found type',type
 		
 		for feature in modelFeatures(".feature-spec .items td").items():
-# 	 		if DEBUG: print 
-# 	 		if DEBUG: print feature.html()
 			label = feature("label").text()
-# 	 		if DEBUG: print 'label:',label
 			
 			if "CURB WEIGHT" in label:
 				curbWeight = feature("span").text()
-# 				if DEBUG: print 'found curb weight',curbWeight,curbWeightRE.findall(curbWeight)
 				curbWeight = curbWeightRE.findall(curbWeight)[0]
 			elif "HORSEPOWER" in label:
 				horsepower = feature("span").text()
-# 				if DEBUG: print 'found horsepower',horsepower,horsepowerRE.findall(horsepower)
 				horsepower = horsepowerRE.findall(horsepower)[0]
 			
-		if DEBUG: print '\ttype:',type, 'curbWeight:',curbWeight, 'horespower',horsepower 
+		if DEBUG: print '\t\ttype:',type, 'curbWeight:',curbWeight, 'horespower',horsepower 
 	except:
-		if DEBUG: print '\tedmunds api error'
+		if DEBUG: print '\t\tedmunds api error'
 	
 	return type, curbWeight, horsepower 
 
@@ -132,8 +307,6 @@ def getMakeModelInfo(makeModel):
 		
 		modelPage = pq(url=link)
 		for modelYear in modelPage('.model-year-summary').items():
-# 				if DEBUG: print 
-# 				if DEBUG: print modelYear.html()
 			
 			year         = modelYear('.summary-year').text().strip()
 			yearAvg      = modelYear('.summary-avg').text().split(' ')[0]
@@ -142,17 +315,9 @@ def getMakeModelInfo(makeModel):
 			yearMiles    = modelYear('.summary-miles').text().strip().split(' ')[0].replace(",", "")
 			yearLink     = modelYear('.summary-view-all-link a').attr('href')
 			
-# 				if yearFuelups < threshold and yearMiles < threshold: return None, None
-			
 			#get model specific info from edmunds api
 			type, weight, hp = api(makeName, modelName, year)
 
-			#store data for this make model and year
-			makeModelInfo.append({'year':year, 'make':makeName, 'model':modelName, 
-								'type':type, 'weight':weight, 'horsepower':hp,
-								'avgMpg':yearAvg, 'fuelups':yearFuelups, 
-								'owners':yearOwners,'miles':yearMiles, 'link':yearLink})
-			
 			modelYearListing = pq(yearLink)
 			
 			pages = [yearLink]
@@ -163,26 +328,6 @@ def getMakeModelInfo(makeModel):
 				modelYearListing = pq(link)
 			
 				for ownerCar in modelYearListing('.browse-by-vehicle-display').items():
-# 						if DEBUG: print 
-# 						if DEBUG: print ownerCar.html()
-					
-					#sample model year item
-					'''
-					<ul class="browse-by-vehicle-display" data-clickable="http://www.fuelly.com/car/abarth/500/2014/deathstalker/285920" style="cursor: pointer;">
-						<li class="browse-image car" style="background-image:url('/photos/000099/r1/15397b5e37028a4.33780631.jpg')">	 </li>
-						<li class="browse-details">
-							<h4>Bianca</h4>
-							<p><strong>2014 Abarth 500  GAS L4 <br><span class="date">Added May 2014</span> - 23 <span>Fuel-ups</span></strong></p>
-							<p>Property of <a href="http://www.fuelly.com/driver/deathstalker" class="profile-link"><span class="fy-icon-avatar"></span> Deathstalker <img src="/img/badges/countries/226.gif"></a></p>
-						</li>
-						<li class="browse-performance ">
-							<div class="vertical-stat">	
-							 
-								<strong>38.2</strong> <em>Avg MPG</em>
-							</div>
-						</li>
-					</ul>
-					'''
 					ownerLink = ownerCar('.browse-by-vehicle-display').attr('data-clickable')
 					country = ownerCar('.browse-details p:last a img').attr('src')
 					country = countryRE.findall(country)[0]
@@ -222,8 +367,6 @@ def getMakeModelInfo(makeModel):
 					location      = ownerPage('.vehicle-info p:last .show .text-muted').text().strip()
 					avgMpg        = ownerPage('.basic-stats.average-mileage .stat-item strong').text()
 					avgCostGallon = ownerPage('.vehicle-costs-list-item.js-average-cost .js-cost-analysis-value').text()
-# 					avgCostFillup = ownerPage('.vehicle-costs-list-item.js-average-fillup .js-cost-analysis-value').text()
-# 					avgCostMile   = ownerPage('.vehicle-costs-list-item.js-average-mile .js-cost-analysis-value').text()
 					fuelUpsCount  = ownerPage('.total-fuelups-count').text().strip().replace(',','')
 					
 					avgCostGallon = decimalRE.findall(avgCostGallon)
@@ -242,19 +385,16 @@ def getMakeModelInfo(makeModel):
 								'avgMpg':avgMpg, 'country':country, 'location':location,
 								'fuelups':fuelUpsCount, 'avgCostGallon':avgCostGallon, 
 								'miles':miles, 'link':ownerLink }
-# 					if DEBUG: print ownerModel
 					ownerModelInfo.append(ownerModel)
 					
 			#save this owner-make-model-year info
 			ownerMakeModelYear = DataFrame(ownerModelInfo)
 	 		ownerMakeModelYear.to_csv(destination+'-owner-makeModel-'+makeName+'-'+modelName+'-info-utf8-.csv',encoding='utf-8')
 	 		print '\tsaved utf8', makeName, modelName, year
+	 		
+	 		if DEBUG: print '\tnationalities:', unique(ownerMakeModelYear['country'])
 				
-		return makeModelInfo, ownerModelInfo
-# 	
-#  		except:
-#  			print 'error in getMakeModelInfo'
-#  			return None, None
+		return ownerModelInfo
 	
 def scrape():
 	
@@ -270,7 +410,6 @@ def scrape():
 	
  	makeModels = []
  	for model in makeModelListing(".models-list li").items():
- 		if DEBUG: print model.html()
  		link = pq(model)('a').attr('href')
  		count = model.html()
  		count = countRE.findall(count)
@@ -284,72 +423,24 @@ def scrape():
 						'count':count})
  		
  	makeModelsDF = DataFrame(makeModels)
- 	makeModelsDF = makeModelsDF.sort(columns='count',ascending=True)
+ 	makeModelsDF = makeModelsDF.sort(columns='count',ascending=False)
+ 	makeModelsDF = makeModelsDF[makeModelsDF['count']<3400]
+	makeModelsDF = makeModelsDF[makeModelsDF['count']>600]
  	
- 	if DEBUG: print
- 	makeModelsDF.to_csv(destination+'-make-models-utf8.csv', encoding='utf-8')
-	makeModelsDF = makeModelsDF[makeModelsDF['count']>500]
-	if DEBUG: makeModelsDF = makeModelsDF.head()
- 	
- 	makeModelInfo = []
  	ownerModelInfo = []
- 	
- 	#resume where we left off scrapping fuelly.com
- 	progressFilename = 'fuellyProgress.txt'
-	f = open(progressFilename, 'r+')
-	lastRecord = safeInt(f.readline())
-	f.close()
- 	
- 	i = -1
  	for index, makeModel in makeModelsDF.iterrows():
- 		i += 1
- 		if (i < lastRecord):
- 			continue
- 		
- 		model, owner = getMakeModelInfo(makeModel)
- 		
+ 		owner = getMakeModelInfo(makeModel)
  		if model is not None:
- 			try:
-		 		df1 = DataFrame(model)
-		 		df2 = DataFrame(owner)
-	 			df1.to_csv(destination+'-make-model-info-utf8-'+`i`+'.csv', encoding='utf-8')
-		 		df2.to_csv(destination+'-owner-model-info-utf8-'+`i`+'.csv',encoding='utf-8')
-		 		print 'saved model info utf8'
- 			except:
- 				print 'error csv WITH utf-8 encoding ',makeModel['makeName'], makeModel['modelName']
- 				
-	 		makeModelInfo.extend(model)
 	 		ownerModelInfo.extend(owner)
 	 		
 	 		try:
-			 	df3 = DataFrame(makeModelInfo)
-				df3.to_csv(destination+'-make-model-info-inprogress.csv', encoding='utf-8')
-				print 'Saved make-model dataframe to csv'
-			 	
-			 	df4 = DataFrame(ownerModelInfo)
-				df4.to_csv(destination+'-owner-model-info-inprogress.csv', encoding='utf-8')
-				print 'Saved owner-model dataframe to csv'
+			 	df = DataFrame(ownerModelInfo)
+				df.to_csv(destination+'-owner-model-info-inprogress.csv', encoding='utf-8')
+				print 'Saved in progress owner-model dataframe to csv'
 			except:
 				print 'error saving running totals', makeModel['makeName'], makeModel['modelName']
 	 	
-	 	#remember where we left off scrapping fuelly.com
-	 	f = open(progressFilename, 'w+')
-		f.write('%d' % i)
-		f.close()
-	 	
- 	if DEBUG: print
- 	if DEBUG: print 'makeModelInfo', len(makeModelInfo)
- 	if DEBUG: print 'ownerModelInfo', len(ownerModelInfo)
- 	
- 	makeModelInfo = DataFrame(makeModelInfo)
-	if DEBUG: print
-	if DEBUG: print makeModelInfo
-	makeModelInfo.to_csv(destination+'-make-model-info.csv', encoding='utf-8')
-	print '\nSaved make-model dataframe to csv'
- 	
  	ownerModelInfo = DataFrame(ownerModelInfo)
-	if DEBUG: print
-	if DEBUG: print ownerModelInfo
 	ownerModelInfo.to_csv(destination+'-owner-model-info.csv', encoding='utf-8')
 	print '\nSaved owner-model dataframe to csv'
  	
